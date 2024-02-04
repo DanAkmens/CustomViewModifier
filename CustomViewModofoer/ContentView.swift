50538a1ef0aaeb5ce526aba8ed97eae6e1b9d04b
Dan <Bdylan998@gmail.com>
2024-02-04 17:15:25 +0200

    views as properties, view composition, view type, order matters

--- CustomViewModofoer/ContentView.swift
+++ CustomViewModofoer/ContentView.swift
@@ -8,14 +8,93 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+    
+    @State private var useRedText = false
+    
+    let motto1 = Text("Dragon Sleeps")
+    let motto2 = Text("Don't tickle")
+    
+    var motto3: some View {
+        Text("Don't panic")
+    }
+    
+    var spells: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+            Text("Lumos")
+            Text("Obliviate")
         }
-        .padding()
+    }
+    
+    @ViewBuilder var spells2: some View {
+        Text("Lumos")
+        Text("Obliviate")
+    }
+    
+    struct CapsuleText: View {
+        var text: String
+        
+        var body: some View {
+            Text(text)
+                .font(.largeTitle)
+                .padding()
+                .background(.blue)
+                .clipShape(.capsule)
+        }
+    }
+    
+    
+    var body: some View {
+        VStack(spacing: 10) {
+            CapsuleText(text: "First")
+                .foregroundStyle(.white)
+            CapsuleText(text:"Second")
+                .foregroundColor(.orange)
+        }
+
+//        If you ever struggle to remember the ternary operator... here's a simple mnemonic.
+//
+//        "WTF"
+//
+//        What? True : False
+//
+//        I mean, you're probably already thinking WTF. Might as well use it.
+
+        Form {
+            
+            VStack {
+                
+                Button("Hello Dainis") {
+                    useRedText.toggle()
+                }
+                .foregroundStyle(useRedText ? .red : .blue)
+                .blur(radius: 5)
+                
+                Text("Danis")
+                    .font(.largeTitle)
+                    .blur(radius: 3)
+                Text("Lada")
+                Text("Katja")
+                Text("Ruslan")
+                    .padding(20)
+            }
+            .font(.title)
+            .blur(radius: 2)
+            
+            VStack(spacing: 20) {
+                motto1
+                    .foregroundStyle(.cyan)
+                motto2
+                    .foregroundStyle(.red)
+                
+                motto3
+                
+                spells
+                    .padding(20)
+                
+                spells2
+                    .foregroundColor(.purple)
+            }
+        }
     }
 }
 
