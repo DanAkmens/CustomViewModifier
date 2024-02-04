3c48ad3ce1c6266e3e6e9b3d85c4ecc188d16ec1
Dan <Bdylan998@gmail.com>
2024-02-04 14:46:15 +0200

    Initial Commit

---
+++ CustomViewModofoer/CustomViewModofoerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomViewModofoerApp.swift
+//  CustomViewModofoer
+//
+//  Created by Dainis Putans on 04/02/2024.
+//
+
+import SwiftUI
+
+@main
+struct CustomViewModofoerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
